347945b0d9e6d8c7de233b3711ffc009ebd66f57
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-09-02 12:11:39 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const dm = document.getElementById('main');
+dm.remove('main');
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+const content = document.createTextNode("Hannah is the champion");
+
+newHeader.appendChild(content);
+document.body.append(newHeader);
